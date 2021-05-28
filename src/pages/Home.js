b2132c34ab99c1d8df217e85ec0d10e864edb419
Bam <vergaraabraham08@gmail.com>
2021-05-28 08:40:53 +0800

    landing page try

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from "../Components/nav";
 
 //import pic from "../images/Screenshot (393).png"
 
 export default function Home() {
+    const [pet, setPet] = useState(null);
+
+    function selectDog(){
+
+        setPet(selectedPet => selectedPet = 'Dog')
+    }
+
+    function selectCat(){
+
+        setPet(selectedPet => selectedPet = 'Cat')
+    }
+
+
     return (
-        <div>
+        <div class = "div1">
             <Nav />
+            <h1> What is your pet? </h1>
+
+            <span class="span1">{pet}</span>
+
+              <button onClick={selectDog}>DOG </button>
+              <button onClick={selectCat}> CAT</button>
+              
+              <button> NEXT</button>
         </div>
     )
 }
